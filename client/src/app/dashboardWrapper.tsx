66a9bad24fbde3f99cc357837c3a9aaa7cddb3ef
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2024-08-12 13:43:47 +0100

    side bar

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,10 +1,11 @@
 import React, { ReactNode } from "react";
 import Navbar from "./(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <div className={`flex bg-gray-50 text-green-900 w-full min-h-screen`}>
-      Sidebar
+      <Sidebar />
       <main
         className={`light flex flex-col w-full h-full py-7 px-9 bg-gray-200 md:pl-24`}
       >
